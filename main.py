c5d203f08b634ba8c5739a107631845de83789b5
jalaxsans08-cmyk <jalaxsans08@gmail.com>
2026-09-16 20:09:33 +0100

    Welcome page

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Welcome to OCRtunes")
+print ("Please create an account for OCRtunes to start")
+username=input("Please enter your chosen username and must be appropriate: ")
+password = input("Please enter a password between 8-20 characters and use some special characters e.g.&@%$: ")
+while len(password) < 8 or len(password) > 20:
+    print("Password must be between 8-20 characters.")
+    password = input("Please enter a password between 8-20 characters and use some special characters e.g.&@%$: ")
+print("Account created successfully!")
